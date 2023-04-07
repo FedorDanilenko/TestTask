919fa86a0a5902fdf5b4c2dd1a4c6e3dea16bbe5
Fedor Danilenko <divine.world.mail@gmail.com>
2023-04-07 21:05:37 +0400

    create CrptApi.java

---
+++ src/main/java/org/example/CrptApi.java
@@ -0,0 +1,99 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import okhttp3.*;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CrptApi {
+
+    private final String baseUrl = "https://markirovka.demo.crpt.tech"; // URL API
+
+    private final OkHttpClient client;
+    private final ObjectMapper objectMapper;
+    private final Lock lock;
+    private final int requestLimit;
+    private final long requestIntervalMillis;
+    private long lastRequestTimeMillis;
+    private int requestCount;
+
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        this.client = new OkHttpClient();
+        this.objectMapper = new ObjectMapper();
+        this.lock = new ReentrantLock();
+        this.requestLimit = requestLimit;
+        this.requestIntervalMillis = timeUnit.toMillis(1);
+        this.lastRequestTimeMillis = System.currentTimeMillis();
+        this.requestCount = 0;
+    }
+
+    public void createDocument(ObjectNode document, String signature) throws Exception {
+        getAsses();
+        checkRequestLimit();
+        ObjectNode requestBody = objectMapper.createObjectNode();
+        requestBody.set("document", document);
+        requestBody.put("signature", signature);
+        String requestJson = objectMapper.writeValueAsString(requestBody);
+        RequestBody requestBodyObject = RequestBody.create(MediaType.parse("application/json"), requestJson);
+        Request request = new Request.Builder()
+                .url(baseUrl + "/lk/documents/commissioning/contract/create")
+                .post(requestBodyObject)
+                .build();
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) throw new Exception("HTTP error code: " + response.code());
+    }
+
+    private void checkRequestLimit() throws InterruptedException {
+        lock.lock();
+        try {
+            long currentTimeLimit = System.currentTimeMillis();
+            long timeSinceLastRequestMils = currentTimeLimit - lastRequestTimeMillis;
+            if (timeSinceLastRequestMils >= requestIntervalMillis) {
+                lastRequestTimeMillis = currentTimeLimit;
+                requestCount = 1;
+            } else {
+                requestCount++;
+                if (requestCount > requestLimit) {
+                    long waitMillis = requestIntervalMillis - timeSinceLastRequestMils;
+                    Thread.sleep(waitMillis);
+                    lastRequestTimeMillis = System.currentTimeMillis();
+                    requestCount = 1;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void getAsses() throws IOException {
+        Request request = new Request.Builder()
+                .url(baseUrl + "/auth/cert/key")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response);
+        String responceBodyString = response.body().string();
+        JsonNode responseJson = objectMapper.readTree(responceBodyString);
+        String uuid = responseJson.get("uuid").asText();
+        String data = responseJson.get("data").asText();
+
+        JSONObject requestJsonBody = new JSONObject();
+        requestJsonBody.put("uuid", uuid);
+        requestJsonBody.put("data", data);
+        RequestBody requestBodyObject = RequestBody.create(MediaType.parse("application/json"), requestJsonBody.toJSONString());
+        request = new Request.Builder()
+                .url(baseUrl + "/auth/cert/")
+                .header("content-type", "application/json;charset=UTF-8")
+                .post(requestBodyObject)
+                .build();
+        response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,91 +1,41 @@
 package org.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import okhttp3.*;
 
-import javax.swing.text.Document;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Main {
     public static void main(String[] args) {
 
         CrptApi crptApi = new CrptApi(TimeUnit.MINUTES, 5);
 
-        String signature = args[1];
-        try (FileReader fileReader = new FileReader(args[0])){
+//        String filePath = args [0];
+//        String signature = args[1];
+
+        String filePath = "./testFile.txt";
+        String signature = "signature";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            String file = sb.toString();
+            ObjectNode doc = (ObjectNode) new ObjectMapper().readTree(file);
+
+            crptApi.createDocument(doc, signature);
+
 
         } catch (Exception e) {
             e.printStackTrace();
+            System.exit(1);
         }
 
-    }
-
-    public static class CrptApi {
-
-        private final String baseUrl = "https://example.com/api"; // здесь указать URL API
-
-        private final OkHttpClient client;
-        private final ObjectMapper objectMapper;
-        private final Lock lock;
-        private final int requestLimit;
-        private final long requestIntervalMillis;
-        private long lastRequestTimeMillis;
-        private int requestCount;
-
-        public CrptApi(TimeUnit timeUnit, int requestLimit) {
-            this.client = new OkHttpClient();
-            this.objectMapper = new ObjectMapper();
-            this.lock = new ReentrantLock();
-            this.requestLimit = requestLimit;
-            this.requestIntervalMillis = timeUnit.toMillis(1);
-            this.lastRequestTimeMillis = System.currentTimeMillis();
-            this.requestCount = 0;
-        }
-
-        public void createDocument (ObjectNode document, String signature) throws Exception {
-            checkRequestLimit();
-            ObjectNode requestBody = objectMapper.createObjectNode();
-            requestBody.set("document", document);
-            requestBody.put("signature", signature);
-            String requestJson = objectMapper.writeValueAsString(requestBody);
-            RequestBody requestBodyObject = RequestBody.create(MediaType.parse("application/json"), requestJson);
-            Request request = new Request.Builder()
-                    .url(baseUrl + "/createDocument")
-                    .post(requestBodyObject)
-                    .build();
-            Response response = client.newCall(request).execute();
-            if (!response.isSuccessful()) throw new Exception("HTTP error code: " + response.code());
-        }
-
-        private void checkRequestLimit() throws InterruptedException {
-            lock.lock();
-            try {
-                long currentTimeLimit = System.currentTimeMillis();
-                long timeSinceLastRequestMils = currentTimeLimit - lastRequestTimeMillis;
-                if (timeSinceLastRequestMils >= requestIntervalMillis) {
-                    lastRequestTimeMillis = currentTimeLimit;
-                    requestCount = 1;
-                } else {
-                    requestCount++;
-                    if (requestCount > requestLimit) {
-                        long waitMillis = requestIntervalMillis - timeSinceLastRequestMils;
-                        Thread.sleep(waitMillis);
-                        lastRequestTimeMillis = System.currentTimeMillis();
-                        requestCount = 1;
-                    }
-                }
-            } finally {
-                lock.unlock();
-            }
-        }
     }
 }
